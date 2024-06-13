b705e076f00789d54e877acafa657b112abd0471
Juliana-Ferreira <juliana_cristina.ferreira@hotmail.com>
2024-06-13 19:58:37 -0300

    Atividade finalizada

--- Linguagens_de_programacao/AF - To Do List/script.js
+++ Linguagens_de_programacao/AF - To Do List/script.js
@@ -1,2 +1,46 @@
 // Juliana Cristina Camargo Ferreira - RA 172008
 
+const button = document.querySelector('.btn');
+const input = document.querySelector('.new-task');
+const completeList = document.querySelector('.list');
+
+let taskList = [];
+
+button.addEventListener('click', function () {
+    taskList.push({
+        task: input.value,
+        checked: false
+    });
+    input.value = '';
+
+    showTasks();
+})
+
+function showTasks() {
+    let newTaskAdded = '';
+
+    taskList.forEach((item, index) => {
+        newTaskAdded += `
+            <li class="task" id="${item.checked && "checkIcon"}">
+                <p>${item.task}</p>
+                <div>
+                    <img class="icon" src="source/check-icon.png" alt="Concluir tarefa" onclick="checkItem(${index})">
+                    <img class="icon" src="source/delete-icon.png" alt="Excluir tarefa" onclick="deleteItem(${index})">
+                </div>
+            </li>`
+    })
+
+    completeList.innerHTML = newTaskAdded;
+}
+
+function deleteItem(index) {
+    taskList.splice(index, 1);
+
+    showTasks();
+}
+
+function checkItem(index) {
+    taskList[index].checked = !taskList[index].checked;
+
+    showTasks();
+}
